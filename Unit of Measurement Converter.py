77cc8e4753a5d0abded1f290df77bf2bb075faca
wbgephart <99994662+wbgephart@users.noreply.github.com>
2024-10-18 11:52:55 -0400

    Add files via upload

---
+++ Unit of Measurement Converter.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+convert_from = input("Enter Starting Unit of Measurement(inches, feet, yards): ")
+convert_to = input("Enter Unit of Measurement to Convert to(inches, feet, yards): ")
+
+if convert_from.lower() in ["inches", "in", "inch"]:
+    number_of_inches = int(input("Enter Starting Measurement in Inches: "))
+    if convert_to.lower() in ["feet", "foot", "ft"]:
+        print("Result: " + str(number_of_inches) + " Inches = " + str(round(number_of_inches / 12, 2)) + " Feet")
+    elif convert_to.lower() in ["yards", "yard", "yrds", "yd", "yrd", "yds"]:
+        print("Result: " + str(number_of_inches) + " Inches = " + str(round(number_of_inches / 36, 2)) + " Yards")
+    else:
+        print("Please Enter either Inches, Feet, or Yards.")
+        
+elif convert_from.lower() in ["feet", "foot", "ft"]:
+    number_of_feet = int(input("Enter Starting Measurement in Feet: "))
+    if convert_to.lower() in ["inches", "in", "inch"]:
+        print("Result: " + str(number_of_feet) + " Feet = " + str(round(number_of_feet * 12)) + " Inches")
+    elif convert_to.lower() in ["yards", "yard", "yrds", "yd", "yrd", "yds"]:
+        print("Result: " + str(number_of_feet) + " Feet = " + str(number_of_feet / 3) + " Yards")
+    else:
+        print("Please Enter either Inches, Feet, or Yards.")
+        
+elif convert_from.lower() in ["yards", "yard", "yrds", "yd", "yrd", "yds"]:
+    number_of_yards = int(input("Enter Starting Measurement in Yards: "))
+    if convert_to.lower() in ["inches", "in", "inch"]:
+        print("Result: " + str(number_of_yards) + " Yards = " + str(round(number_of_yards * 36)) + " Inches")
+    elif convert_to.lower() in ["feet", "foot", "ft"]:
+        print("Result: " + str(number_of_yards) + " Yards = " + str(round(number_of_yards * 3)) + " Feet")
+    else:
+        print("Your input was incorrect")
+else:
+    print("Please Enter either Inches, Feet, or Yards.")
+
